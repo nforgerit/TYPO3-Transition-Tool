024bf29d80a14002e6e1aa2e21ad93b90f86d24c
Karsten Dambekalns <karsten@typo3.org>
2013-09-11 17:47:55 +0200

    Fix call-time pass-by-reference and do some cleanup
    Call-time pass-by-reference has been removed in PHP 5.4, so solve the
    task differently.
    
    Does some code cleanup along the way.

--- Classes/Domain/Model/Data/Transformator.php
+++ Classes/Domain/Model/Data/Transformator.php
@@ -1,159 +1,183 @@
 <?php
 
 class Tx_T3tt_Domain_Model_Data_Transformator {
-    
-    protected $_requestInserted = FALSE;
-    
-    protected $_dataTransformed = FALSE;
-    
-    protected $_transformationRequest = NULL;
-    
-    protected $_phpHookAggregation = array();
-    
-    protected $_xsltStylesheet = NULL;
-    
-    protected $_xsltProcessor = NULL;
-    
-    protected $_data = NULL;
-    
-    
-    public function setRequest(Tx_T3tt_Domain_Model_Request_DataTransformationRequest $transformationRequest) {
-        $this->_transformationRequest = $transformationRequest;
-        $this->_requestInserted = TRUE;
-        return $this;
-    }
-     
-    public function exec() {        
-        if (! $this->_requestInserted) {
-            throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Missing the transformation request. You need to inject it via setRequest().");
-        }
-        $this->prepare();
-        $this->transform();
-        $this->_dataTransformed = TRUE;
-        return $this;
-    }
-        
-    public function getData() {
-        if (! $this->_dataTransformed) {
-            throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Transformation was never executed. You need to call exec() first.");
-        }
 
-        return (string) $this->_data;
-    }
-    
-    public function writeDataToOutputFile() {
-        $outputFile = t3lib_extMgm::extPath('t3tt').'Resources/Private/Data/Output.xml';
-        $returnValue = file_put_contents($outputFile, $this->getData());
-        
-        if ($returnValue > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    private function prepare() {
-        $this->fetchInitialData();
-        $this->setXsltProcessor();
-        $this->fetchXsltStylesheet();
-        $this->substituteMarkersWithValues();
-        $this->fetchPhpHooks();
-    }
-    
-    private function transform() {
-        $this->execPhpHooks('pre');    
-        $this->execXsltTransformation();
-        $this->execPhpHooks('post');
-    }
-    
-    private function fetchInitialData() {
-        $filedir = t3lib_extMgm::extPath('t3tt').'Resources/Private/Data/'.$this->_transformationRequest->getInputDatafile();
-        if (! file_exists($filedir)) {
-            throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given Input data file `".$filedir."' does not exist.");
-        }
-        
-        $this->_data = file_get_contents($filedir);
-    }
-    
-    private function setXsltProcessor() {
-        $xsltProcessorClass = "Tx_T3tt_Domain_Service_".$this->_transformationRequest->getXsltProcessor();
-        
-        if (! class_exists($xsltProcessorClass)) {
-            throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given XSLT Processor class does not exist.");
-        }
-        
-        $this->_xsltProcessor = new $xsltProcessorClass;
-    }
-    
-    private function fetchXsltStylesheet() {
-        $compositor = new Tx_T3tt_Domain_Model_Xslt_StylesheetComposition;
-        $this->_xsltStylesheet = $compositor
-            ->setXsltStylesheetAggregation(new Tx_T3tt_Domain_Model_Xslt_StylesheetAggregation)
-            ->setBaseXsltStylesheet(t3lib_extMgm::extPath('t3tt').'Resources/Private/XSLT/Stylesheets/master.xsl') // TODO: make this configurable
-            ->getCompound();
-            
-        if (! $this->_xsltStylesheet) {
-            throw new Tx_T3tt_Domain_Model_Exception_InvalidXsltStylesheetContentsException("Constructing XSLT stylesheet failed :(");
-        }
-    }
-    
-    private function fetchPhpHooks() {
-        $hooksFileBasepath = t3lib_extMgm::extPath('t3tt').'Resources/Private/PHP/Transition/' ;// TODO: make this configurable
-		$hooksFile = $hooksFileBasepath.$this->_transformationRequest->getPhpHooksFile();
-    	$this->_phpHookAggregation = (array) include_once ($hooksFile); 
-    }
-    
-    private function execPhpHooks($category) {
-        if (empty($this->_phpHookAggregation[$category])) {
-            throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given category `".$category."' does not exist in PHP Hooks array.");
-        }
-        
-        if (! is_array($this->_data)) {
+	/**
+	 * @var boolean
+	 */
+	protected $_requestInserted = FALSE;
+
+	/**
+	 * @var boolean
+	 */
+	protected $_dataTransformed = FALSE;
+
+	/**
+	 * @var Tx_T3tt_Domain_Model_Request_DataTransformationRequest
+	 */
+	protected $_transformationRequest = NULL;
+
+	/**
+	 * @var array
+	 */
+	protected $_phpHookAggregation = array();
+
+	/**
+	 * @var string
+	 */
+	protected $_xsltStylesheet = NULL;
+
+	/**
+	 * @var Tx_T3tt_Domain_Service_AbstractXsltProcessor
+	 */
+	protected $_xsltProcessor = NULL;
+
+	/**
+	 * @var string
+	 */
+	protected $_data = NULL;
+
+	public function setRequest(Tx_T3tt_Domain_Model_Request_DataTransformationRequest $transformationRequest) {
+		$this->_transformationRequest = $transformationRequest;
+		$this->_requestInserted = TRUE;
+
+		return $this;
+	}
+
+	public function exec() {
+		if (!$this->_requestInserted) {
+			throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Missing the transformation request. You need to inject it via setRequest().");
+		}
+		$this->prepare();
+		$this->transform();
+		$this->_dataTransformed = TRUE;
+
+		return $this;
+	}
+
+	public function getData() {
+		if (!$this->_dataTransformed) {
+			throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Transformation was never executed. You need to call exec() first.");
+		}
+
+		return (string)$this->_data;
+	}
+
+	public function writeDataToOutputFile() {
+		$outputFile = t3lib_extMgm::extPath('t3tt') . 'Resources/Private/Data/Output.xml';
+		$returnValue = file_put_contents($outputFile, $this->getData());
+
+		if ($returnValue > 0) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+
+	private function prepare() {
+		$this->fetchInitialData();
+		$this->setXsltProcessor();
+		$this->fetchXsltStylesheet();
+		$this->substituteMarkersWithValues();
+		$this->fetchPhpHooks();
+	}
+
+	private function transform() {
+		$this->execPhpHooks('pre');
+		$this->execXsltTransformation();
+		$this->execPhpHooks('post');
+	}
+
+	private function fetchInitialData() {
+		$filedir = t3lib_extMgm::extPath('t3tt') . 'Resources/Private/Data/' . $this->_transformationRequest->getInputDatafile();
+		if (!file_exists($filedir)) {
+			throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given Input data file `" . $filedir . "' does not exist.");
+		}
+
+		$this->_data = file_get_contents($filedir);
+	}
+
+	private function setXsltProcessor() {
+		$xsltProcessorClass = "Tx_T3tt_Domain_Service_" . $this->_transformationRequest->getXsltProcessor();
+
+		if (!class_exists($xsltProcessorClass)) {
+			throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given XSLT Processor class does not exist.");
+		}
+
+		$this->_xsltProcessor = new $xsltProcessorClass;
+	}
+
+	private function fetchXsltStylesheet() {
+		$compositor = new Tx_T3tt_Domain_Model_Xslt_StylesheetComposition;
+		$this->_xsltStylesheet = $compositor
+			->setXsltStylesheetAggregation(new Tx_T3tt_Domain_Model_Xslt_StylesheetAggregation)
+			->setBaseXsltStylesheet(t3lib_extMgm::extPath('t3tt') . 'Resources/Private/XSLT/Stylesheets/master.xsl') // TODO: make this configurable
+			->getCompound();
+
+		if (!$this->_xsltStylesheet) {
+			throw new Tx_T3tt_Domain_Model_Exception_InvalidXsltStylesheetContentsException("Constructing XSLT stylesheet failed :(");
+		}
+	}
+
+	private function fetchPhpHooks() {
+		$hooksFileBasepath = t3lib_extMgm::extPath('t3tt') . 'Resources/Private/PHP/Transition/'; // TODO: make this configurable
+		$hooksFile = $hooksFileBasepath . $this->_transformationRequest->getPhpHooksFile();
+		$this->_phpHookAggregation = (array)include_once($hooksFile);
+	}
+
+	private function execPhpHooks($category) {
+		if (empty($this->_phpHookAggregation[$category])) {
+			throw new Tx_T3tt_Domain_Model_Exception_InvalidParamsException("Given category `" . $category . "' does not exist in PHP Hooks array.");
+		}
+
+		if (!is_array($this->_data)) {
 			$lines = explode("\n", $this->_data);
 		} else {
 			$lines = $this->_data;
-		}                               
-		
-		foreach ($this->_phpHookAggregation[$category] as $func_name => $func) {
-			$output = call_user_func($func, &$lines);
-		}                    
-		
-        if (! $output) return;
-		                                   
+		}
+
+		$output = NULL;
+		foreach ($this->_phpHookAggregation[$category] as $func) {
+			$output = $func($lines);
+		}
+
+		if ($output === NULL) {
+			return;
+		}
+
 		if (is_array($output)) {
-		    // TODO: \n may be critical concerning system independency, see how F3 handles that (some chr() stuff)
-			$output = implode("\n", $output); 
-		}                                 
+			// TODO: \n may be critical concerning system independency, see how F3 handles that (some chr() stuff)
+			$output = implode("\n", $output);
+		}
 
 		$this->_data = $output;
-    }
-    
-    private function execXsltTransformation() {
-        $stylesheetDomObj = DOMDocument::loadXML($this->_xsltStylesheet);
-        if (! $stylesheetDomObj) {
-            throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Assembled stylesheet could not be transformed into a DOMDocument.");
-        }
-        
-        $dataDomObj = DOMDocument::loadXML($this->_data);
-        if (! $dataDomObj) {
-            throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Prepared data could not be transformed into a DOMDocument.");
-        }
-        
-        /* THIS IS THE SWEET SPOT */               
-		$this->_data = $this->_xsltProcessor
-		    ->setStylesheet($stylesheetDomObj)
-            ->setInputData($dataDomObj)
-            ->render();
-    }
-    
-    private function substituteMarkersWithValues() {
-        $markerArray = $this->_transformationRequest->getMarkerData();
-        foreach ($markerArray as $marker => $value) {
-            $marker = "<!-- {$marker} -->";
-            $this->_xsltStylesheet = str_replace($marker, $value, $this->_xsltStylesheet);            
-        }
-    }
-    
-    
+	}
 
+	private function execXsltTransformation() {
+		$stylesheetDomObj = new DOMDocument();
+		$stylesheetDomObj->loadXML($this->_xsltStylesheet);
+		if (!$stylesheetDomObj) {
+			throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Assembled stylesheet could not be transformed into a DOMDocument.");
+		}
+
+		$dataDomObj = new DOMDocument();
+		$dataDomObj->loadXML($this->_data);
+		if (!$dataDomObj) {
+			throw new Tx_T3tt_Domain_Model_Exception_NoParamsException("Prepared data could not be transformed into a DOMDocument.");
+		}
+
+		/* THIS IS THE SWEET SPOT */
+		$this->_data = $this->_xsltProcessor
+			->setStylesheet($stylesheetDomObj)
+			->setInputData($dataDomObj)
+			->render();
+	}
+
+	private function substituteMarkersWithValues() {
+		$markerArray = $this->_transformationRequest->getMarkerData();
+		foreach ($markerArray as $marker => $value) {
+			$marker = "<!-- {$marker} -->";
+			$this->_xsltStylesheet = str_replace($marker, $value, $this->_xsltStylesheet);
+		}
+	}
 }
--- Classes/Domain/Service/PhpXsltProcessor.php
+++ Classes/Domain/Service/PhpXsltProcessor.php
@@ -2,68 +2,83 @@
 
 class Tx_T3tt_Domain_Service_PhpXsltProcessor extends Tx_T3tt_Domain_Service_AbstractXsltProcessor {
 
-    	/**
-    	 * The input data feeded to the XSLT processor
-    	 * @var String (XML) $_inputdata
-    	 */
-    	private $_inputData;
+	/**
+	 * The input data feeded to the XSLT processor
+	 *
+	 * @var string (XML) $_inputdata
+	 */
+	private $_inputData;
 
-    	/**
-    	 * The resulting output data of the XSLT transformation
-    	 * @var String (XML) $_outputData
-    	 */
-    	private $_outputData;
+	/**
+	 * The resulting output data of the XSLT transformation
+	 *
+	 * @var string (XML) $_outputData
+	 */
+	private $_outputData;
 
-    	/**
-    	 * The XSLT stylesheet used by the XSLT processor
-    	 * @var String (XSLT) $_styleSheet
-    	 */
-    	private $_styleSheet;
+	/**
+	 * The XSLT stylesheet used by the XSLT processor
+	 *
+	 * @var string (XSLT) $_styleSheet
+	 */
+	private $_styleSheet;
 
-    	private $_profilerLogPath;
+	/**
+	 * @var string
+	 */
+	private $_profilerLogPath;
 
-    	/**
-    	 * This function simply injects the input data
-    	 * @param DOMDocument $inp The input data injected into the XSLTProcessor service class
-    	 * @access public
-    	 */
-    	public function setInputData(DOMDocument $inp) {
-    		$this->_inputData = $inp;
-    		return $this;
-    	}                                                      
+	/**
+	 * This function simply injects the input data
+	 *
+	 * @param DOMDocument $inp The input data injected into the XSLTProcessor service class
+	 * @return Tx_T3tt_Domain_Service_PhpXsltProcessor
+	 */
+	public function setInputData(DOMDocument $inp) {
+		$this->_inputData = $inp;
 
-    	/**
-    	 * This function simply injects the stylesheet data
-    	 * @param DOMDocument $xsls The stylesheet data injected into the XSLTProcessor service class
-    	 * @access public
-    	 */	
-    	public function setStylesheet(DOMDocument $xsls) {
-    		$this->_styleSheet = $xsls;
-    		return $this;
-    	}
+		return $this;
+	}
 
-    	public function setProfiler($logPath) {
-    	    $this->_profilerLogPath = $logPath;
-    	    return $this;
-    	}
+	/**
+	 * This function simply injects the stylesheet data
+	 *
+	 * @param DOMDocument $xsls The stylesheet data injected into the XSLTProcessor service class
+	 * @return Tx_T3tt_Domain_Service_PhpXsltProcessor
+	 */
+	public function setStylesheet(DOMDocument $xsls) {
+		$this->_styleSheet = $xsls;
 
-    	/**
-    	 * The main function of this object which simply renders the accumulated data using PHP's XSLTProcessor
-    	 * @access public
-    	 */
-    	public function render() {
-    		try {
-    			$xsltproc = new \XSLTProcessor;
-    			$xsltproc->importStylesheet($this->_styleSheet);
+		return $this;
+	}
 
-    			if (isset($this->_profilerLogPath)) {
-    			    $xsltproc->setProfiling($this->_profilerLogPath);
-    			}
+	/**
+	 * @param $logPath
+	 * @return Tx_T3tt_Domain_Service_PhpXsltProcessor
+	 */
+	public function setProfiler($logPath) {
+		$this->_profilerLogPath = $logPath;
 
-    			return $xsltproc->transformToXML($this->_inputData);			
-    		} catch (Exception $e) {
-    			echo "Could not process the Datafile with the given Stylesheet: " . $e->getMessage();
-    		}
-    	}
+		return $this;
+	}
 
-    }
+	/**
+	 * The main function of this object which simply renders the accumulated data using PHP's XSLTProcessor
+	 *
+	 * @return string
+	 */
+	public function render() {
+		try {
+			$xsltproc = new \XSLTProcessor;
+			$xsltproc->importStylesheet($this->_styleSheet);
+
+			if (isset($this->_profilerLogPath)) {
+				$xsltproc->setProfiling($this->_profilerLogPath);
+			}
+
+			return $xsltproc->transformToXML($this->_inputData);
+		} catch (Exception $e) {
+			echo "Could not process the Datafile with the given Stylesheet: " . $e->getMessage();
+		}
+	}
+}
